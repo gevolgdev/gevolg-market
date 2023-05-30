5f95e769e3183a3e238fcd215164a1f74cfd802c
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-30 19:42:26 -0300

    React Route rDom installed/config

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import { useState } from 'react';
-import { AppContainer } from './style';
-
-interface Infos {
-  name: string;
-  age: number;
-}
+import { Outlet } from "react-router-dom";
+import { GlobalStyle, Wrapper } from './style/GlobalStyle';
 
 function App() {
-  const [infos, setInfos] = useState<Infos | null>(null);
-
-  const name: string = 'Davi'
-
-  const Name = completeName('Ricardo', 23)
-
-  function completeName(name: string, age: number) {
-    return <h1>Meu nome é {name} e tenho {age} anos</h1>
-  }
-
   return (
-    <AppContainer>
-      <h1>React.js + Typescript + Redux</h1>
-      {Name}
-    </AppContainer>
+    <>
+      <Wrapper>
+        <Outlet/>
+      </Wrapper>
+      <GlobalStyle/>
+    </>
   )
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { Initial, Home } from './routes';
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>,
+      },
+      {
+        path: '/initial',
+        element: <Initial/>,
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
