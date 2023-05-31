5ccbf9d052146dabf7eee8cc8106e70f2b03d012
Pinelli <ricardosilvapinelli@gmail.com>
2023-05-31 01:56:49 -0300

    Props e tipos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,14 +2,18 @@ import { BoxName, Container, Cart } from './style';
 import { MdOutlineShoppingCart } from 'react-icons/md';
 import LOGO from '../../assets/logo-dark.png';
 
-const Header = () => {
+interface NameProps {
+  name: string;
+};
 
-  const name: string = 'Ricardo'.charAt(0);
+const Header:React.FC<NameProps> = ({name}) => {
+
+  const letterName: string = name.charAt(0);
   const stock: number[] = [1];
 
   return (
     <Container>
-      <BoxName>{name}</BoxName>
+      <BoxName>{letterName}</BoxName>
       <img src={LOGO}/>
       <Cart>
         {stock.length > 0 && <div className="haveItems"/>}
