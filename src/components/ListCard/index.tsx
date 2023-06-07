0dc0c9a3465714c8927cafc445d16f3f1de46367
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-07 20:12:30 -0300

    ListCard: openList Hook

--- src/components/ListCard/index.tsx
+++ src/components/ListCard/index.tsx
@@ -2,13 +2,19 @@ import { MdArrowOutward } from 'react-icons/md';
 import { HiOutlineMinus } from 'react-icons/hi';
 import { ListProps } from '../../types/types';
 import { Container } from './style';
+import useCreateLists from '../../hooks/useCreateLists';
 
-const ListCard: React.FC<ListProps> = ({title, priority, color}) => {
+const ListCard: React.FC<ListProps & {index: number}> = ({
+  title, 
+  priority, 
+  color, 
+  index }) => {
 
   const currentDate: string = new Date().toLocaleDateString();
+  const { openList } = useCreateLists();
 
   return (
-    <Container color={color}>
+    <Container color={color} onClick={() => openList(index)}>
       <div className="header">
         <span>{priority}</span>
         <MdArrowOutward/>
