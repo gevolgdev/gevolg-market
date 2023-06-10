62be44955f569cccce8c7c3cb5e0b30fbe07eaab
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-10 18:37:12 -0300

    Types: Products[]

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,9 +1,17 @@
+
+export interface ProductsProps {
+  name: string;
+  amount: number;
+  collected: boolean;
+};
+
 export interface ListProps {
   index?: number;
   id?: number,
   priority: string;
   title: string;
   color: string;
+  products?: ProductsProps[];
 };  
 
 export type EventProps = React.ChangeEvent<HTMLInputElement | HTMLSelectElement>;
