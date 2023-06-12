aff6e6994cc3dc798550bc85ba791b121452f77c
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-12 01:24:18 -0300

    BodyList: DeleteContainer implementado

--- src/components/BodyList/index.tsx
+++ src/components/BodyList/index.tsx
@@ -1,36 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useLocation } from 'react-router-dom'
 import ButtonBack from '../ButtonBack';
 import { Container, Header, Content } from './style';
 import Logo from '../../assets/logo-light.svg';
 import { AiFillDelete } from 'react-icons/ai';
-import deleteList from '../../utils/deleteList';
+import DeleteContainer from './DeleteContainer';
 
 const BodyList: React.FC = () => {
+
+  const [confirmDelete, setConfirmDelete] = useState<boolean>(false);
 
   const location = useLocation();
   const { newCurrentPage } = location?.state;
   const {color, priority, index, title} = newCurrentPage;
 
-  const { deleted } = deleteList(newCurrentPage.index);
-
   return (
     <>
-      <Header color={color}>
+      { confirmDelete && <DeleteContainer 
+        index={index} 
+        setConfirmDelete={setConfirmDelete} 
+        title={title}
+      /> }
+
+      <Header color={ color }>
         <ButtonBack/>
         <img src={Logo}/>
-        <button className='delete' onClick={deleted}><AiFillDelete/></button>
+        <button className='delete' onClick={ () => setConfirmDelete(true) }>
+          <AiFillDelete/>
+        </button>
       </Header>
       <Container>
-
         <Content>
           <div className="header">
             <p>Prioridade {priority.toLowerCase()}</p>
             <h1><span>{index + 1}.</span> {title}</h1>
           </div>
         </Content>
-        
       </Container>
+      
     </>
   );
 };
