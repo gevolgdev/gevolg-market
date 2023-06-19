cd9acadfb6c926e9340b666da267629309092f52
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-19 12:17:06 -0300

    ButtonBack: tema dinamico

--- src/components/ListInside/ButtonBack/index.tsx
+++ src/components/ListInside/ButtonBack/index.tsx
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { BsArrowLeftShort } from 'react-icons/bs';
 import { Container } from './style';
 
-const ButtonBack = () => {
+interface ButtonProps {
+  mode: string;
+};
+
+const ButtonBack: React.FC<ButtonProps> = ({ mode }) => {
+
+  // const [ color, setColor ] = useState<string>('');
+
+  const setModeColor = (el: string): string => {
+    let color: string = '';
+    if(el === 'light') {
+      color = '#FAF8F0';
+    } 
+    else if(el === 'dark') {
+      color = '#19191F';
+    };
+
+    return color;
+  };
+
 
   const navigate = useNavigate();
 
   return (
-    <Container onClick={() => navigate(-1)}>
+    <Container onClick={() => navigate(-1)} color={setModeColor(mode)}>
       <BsArrowLeftShort/>
     </Container>
   );
--- src/components/ListInside/ButtonBack/style.ts
+++ src/components/ListInside/ButtonBack/style.ts
@@ -1,11 +1,14 @@
 import styled from "styled-components";
 
+interface ContainerProps {
+  color: string;
+};
 
-export const Container = styled.button`
+export const Container = styled.button<ContainerProps>`
   display: flex;
   background: transparent;
   border: none;
   font-size: 2rem;
   cursor: pointer;
-  color: white;
+  color: ${({ color }) => color};
 `;
