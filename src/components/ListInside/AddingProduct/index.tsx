ba25ea811715812578df5d63710392f11889fef4
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-19 22:53:22 -0300

    AddingProduct: Componente pronto

--- src/components/ListInside/AddingProduct/index.tsx
+++ src/components/ListInside/AddingProduct/index.tsx
@@ -1,18 +1,46 @@
 import React, { Dispatch } from 'react'
-import { Container } from './style';
-import addProductList from '../../../utils/addProductList';
+import { Container, Button, Header, Inputs } from './style';
 import { Wrapper } from '../../../style/GlobalStyle';
+import Logo from '../../../assets/logo-light.svg';
+import { GrFormClose } from 'react-icons/gr';
+import addProductList from '../../../utils/addProductList';
 
 interface AddingProductProps {
-  setProductForm: Dispatch<React.SetStateAction<boolean>>
+  setOpenAddProduct: Dispatch<React.SetStateAction<boolean>>;
+  index: number;
 };
 
-const AddingProduct: React.FC<AddingProductProps> = ({ setProductForm }) => {
+const AddingProduct: React.FC<AddingProductProps> = ({ setOpenAddProduct, index }) => {
+
+  const { addProduct, saveInfosProduct } = addProductList(index);
   
   return (
     <Container>
       <Wrapper>
-        <button onClick={ () => setProductForm(false) }>✕</button>
+        <Header>
+          <img src={Logo}/>
+          <button onClick={ () => setOpenAddProduct(false) }>
+            <GrFormClose color='#FAF8F0'/>
+          </button>
+        </Header>
+
+        <Inputs>
+          <h1>Adicione um produto</h1>
+          <div>
+            <label>Título do Produto</label>
+            <input id='title' type='text' onChange={saveInfosProduct}/>
+          </div>
+          <div>
+            <label>Quantidade</label>
+            <input id='amount' type='number' onChange={saveInfosProduct}/>
+          </div>
+          <div>
+            <label>Seção</label>
+            <input id='section' type='text' onChange={saveInfosProduct}/>
+          </div>
+        </Inputs>
+
+        <Button onClick={() => (addProduct(), setOpenAddProduct(false))}>Adicionar</Button>
       </Wrapper>
     </Container>
   );
--- src/components/ListInside/AddingProduct/style.ts
+++ src/components/ListInside/AddingProduct/style.ts
@@ -1,11 +1,80 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
   position: absolute;
   left: 0;
   width: 100%;
   height: 100vh;
+  padding: 1.5rem;
   background: #9B7BFF;
   z-index: 99;
+`;
 
+export const Inputs = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 1.5rem;
+  gap: 1rem;
+
+  h1 {
+    font-size: 2rem;
+    margin: 2rem 0 1rem;
+    color: #FAF8F0;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      color: #D9D9D9;
+      margin-bottom: 0.5rem;
+      font-size: 0.9rem;
+    }
+    input, select {
+      font-size: 1rem;
+      font-family: 'Inter Tight', sans-serif;
+      background: #F1F1F1;
+      border-radius: 5px;
+      border: none;
+      outline: none;
+      padding: 15px;
+    }
+  }         
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  margin-left: auto;
+  padding: 1rem 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  button {
+    display: flex;
+    align-items: start;
+    font-size: 2rem;
+    background: transparent;
+    border: none;
+    color: #FAF8F0;
+    cursor: pointer;
+  }                                             
+`;
+
+export const Button = styled.button`
+  width: 90%;
+  display: flex;
+  justify-content: center;
+  font-family: 'Inter Tight', sans-serif;
+  font-weight: bold;
+  font-size: 1rem;
+  margin: 50px auto 0;
+  padding: 0.9rem;
+  border-radius: 100px;
+  background: #51FFB0;
+  border: none;
+  cursor: pointer;
 `;
