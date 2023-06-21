470c4583408d1e601eb73ea01a243087b55af463
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-21 03:35:00 -0300

    GlobalStyle: html > button

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -3,6 +3,13 @@ import styled, { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
   * {margin: 0; padding: 0; box-sizing: border-box}
 
+  html {
+    button {
+      font-family: 'Inter Tight', sans-serif;
+      cursor: pointer;
+    }
+  }
+
   body {
     font-family: 'Inter Tight', sans-serif;
     font-size: 16px;
