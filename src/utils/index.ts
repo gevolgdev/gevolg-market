2f657198fc9075bea9411dca9d0717899707c75a
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-21 03:36:07 -0300

    Utils: Index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,8 @@
-import addProductList from "./addProductList";
 import deleteList from "./deleteList";
 import systemSearch from "./systemSearch";
 
 
 export {
-  addProductList,
   deleteList,
   systemSearch,
 };
