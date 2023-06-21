29e68ce6cd11d727cc8dd5cb3863745f89af6911
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-21 14:16:26 -0300

    ListInside: Index

--- src/components/ListInside/index.ts
+++ src/components/ListInside/index.ts
@@ -1,10 +1,12 @@
 import AddingProduct from "./AddingProduct";
 import DeleteContainer from "./DeleteContainer";
 import ButtonBack from "./ButtonBack";
+import ListDetails from "./ListDetails";
 
 
 export {
   AddingProduct,
   DeleteContainer,
   ButtonBack,
+  ListDetails,
 };
