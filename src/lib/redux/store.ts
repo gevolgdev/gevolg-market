9e1b4913b100b86a07f0c1242743466d960cf3c6
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-21 17:20:46 -0300

    Teste: LocalStotage

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducer';
+import { loadState, saveState } from '../../utils/localStorage';
+
+const persistedState = loadState();
 
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export default store;
