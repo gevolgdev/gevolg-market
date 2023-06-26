63d17d49c84ef9e127e27cbbcef530b495cc811c
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-26 12:34:19 -0300

    Reducer: setNameSlice

--- src/lib/redux/reducer.ts
+++ src/lib/redux/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import listsSlice from './slices/listsSlice';
 import showModalSlice from './slices/showModalSlice';
+import setNameSlice from './slices/setNameSlice';
 
 export const rootReducer = combineReducers({
   listsSlice,
   showModalSlice,
+  setNameSlice,
 });
 
 export type RootState = ReturnType<typeof rootReducer>
