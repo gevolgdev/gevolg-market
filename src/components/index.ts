9db82ace4c1f43cf00032b1fd36d762bac679cbb
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-26 12:32:57 -0300

    Index: UserSetName

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import Header from './Header';
 import Lists from './Lists';
 import ListCard from './ListCard';
 import AddList from './AddList';
+import UserSetName from './UserSetName';
 
 export {
   Header,
   Lists,
   ListCard,
   AddList,
+  UserSetName,
 }
