01673eb943ecc980f1f57f6605e37f754032c281
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-29 16:35:27 -0300

    EditingProduct: Componente

---
+++ src/components/List/EditingProduct/index.tsx
@@ -0,0 +1,67 @@
+import React, { Dispatch, useState } from 'react'
+import { ContainerEdit, Button, HeaderEdit, InputsEdit } from './style';
+import { MdClose } from 'react-icons/md';
+import useProduct from '../../../hooks/useProduct';
+import { dataCategories } from './dataCategories';
+import { EditInfosProps, EventProps } from '../../../types/types';
+
+interface EditingProductProps {
+  indexMain: number;
+  indexChild: number;
+  setOpenEdit: Dispatch<React.SetStateAction<boolean>>;
+  title: string;
+  amount: number;
+  category: string;
+};
+
+const EditingProduct: React.FC<EditingProductProps> = ({ setOpenEdit, indexMain, indexChild, title, amount, category }) => {
+
+  const { edit } = useProduct(indexMain);
+
+  const INITIAL_VALUE: EditInfosProps = 
+    { title, amount, category, }
+  ;
+
+  const [editInfosProduct, setEditInfosProduct] = useState<EditInfosProps>(INITIAL_VALUE);
+
+  const saveEditInfosProduct = (e: EventProps) => {
+    const { id, value } = e.target;
+    setEditInfosProduct(prev => ({...prev, [id]: value}));
+  };
+  
+  return (
+    <ContainerEdit>
+      <HeaderEdit>
+        <h1>Edite um<br/> produto</h1>
+        <button onClick={ () => setOpenEdit(false) }>
+          <MdClose/>
+        </button>
+      </HeaderEdit>
+
+      <InputsEdit>
+        <div>
+          <label>Título do Produto</label>
+          <input id='title' type='text' onChange={saveEditInfosProduct} value={editInfosProduct.title}/>
+        </div>
+        <div>
+          <label>Quantidade</label>
+          <input id='amount' type='number' onChange={saveEditInfosProduct} value={editInfosProduct.amount}/>
+        </div>
+        <div>
+          <label>Categoria</label>
+          {/* <input id='category' type='text' onChange={saveEditInfosProduct}/> */}
+          <select onChange={saveEditInfosProduct} id='category' value={editInfosProduct.category}>
+            <option value='--'>--</option>
+            {dataCategories.sort().map(item => (
+              <option value={item}>{item}</option>
+            ))}
+          </select>
+        </div>
+      </InputsEdit>
+
+      <Button onClick={() => (edit(indexMain, indexChild, editInfosProduct), setOpenEdit(false))}>Editar</Button>
+    </ContainerEdit>
+  );
+};
+
+export default EditingProduct;
---
+++ src/components/List/EditingProduct/style.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+export const ContainerEdit = styled.section`
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  padding: 3rem;
+  background: #9B7BFF;
+  z-index: 99;
+`;
+
+export const InputsEdit = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 3rem;
+  gap: 1rem;
+
+  div {
+    display: flex;
+    flex-direction: column;
+
+    label {
+      color: #D9D9D9;
+      margin-bottom: 0.5rem;
+      font-size: 0.9rem;
+    }
+    input, select {
+      font-size: 1rem;
+      font-family: 'Inter Tight', sans-serif;
+      background: #F1F1F1;
+      border-radius: 5px;
+      border: none;
+      outline: none;
+      padding: 15px;
+    }
+  }         
+`;
+
+export const HeaderEdit = styled.header`
+  width: 100%;
+  margin-left: auto;
+  padding: 1rem 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 2rem;
+    color: #FAF8F0;
+  }
+
+  button {
+    display: flex;
+    align-items: start;
+    font-size: 1.3rem;
+    background: white;
+    padding: 0.3rem;
+    border: none;
+    border-radius: 7px;
+    color: #dc2626;
+    cursor: pointer;
+  }                                             
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-family: 'Inter Tight', sans-serif;
+  font-weight: bold;
+  font-size: 1rem;
+  margin: 50px auto 0;
+  padding: 0.9rem;
+  border-radius: 100px;
+  background: #51FFB0;
+  border: none;
+  cursor: pointer;
+`;
