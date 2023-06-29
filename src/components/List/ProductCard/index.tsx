5033655190b093afbffe57083fb0cf1fd02a738f
Pinelli <ricardosilvapinelli@gmail.com>
2023-06-29 16:37:42 -0300

    ProductCard: Componente Editing

--- src/components/List/ProductCard/index.tsx
+++ src/components/List/ProductCard/index.tsx
@@ -1,7 +1,9 @@
+import { Dispatch, useState } from 'react';
 import { BsCheck } from 'react-icons/bs';
 import { Product } from './style';
 import Options from '../Options';
 import useProduct from '../../../hooks/useProduct';
+import EditingProduct from '../EditingProduct';
 
 interface ProductCardProps {
   isCollected: boolean;
@@ -16,19 +18,30 @@ interface ProductCardProps {
 const ProductCard = ({ isCollected, title, amount, category, options, i, index }: ProductCardProps) => {
 
   const { collected } = useProduct(index);
+  const [openEdit, setOpenEdit] = useState<boolean>(false);
   
   return (
-    <Product collected={isCollected} key={title}>
-      <div>
-        <button className='checkbox' onClick={ () => collected(i) }>
-          {isCollected && <BsCheck/>}
-        </button>
-        <h1>( {amount === 0 ? '1' : amount} ) {title} - <span>{category}</span></h1>
-      </div>
+    <>
+      {openEdit && <EditingProduct 
+        setOpenEdit={setOpenEdit}
+        indexMain={index}
+        indexChild={i}
+        title={title}
+        amount={amount}
+        category={category}
+      />}
+      <Product collected={isCollected} key={title}>
+        <div>
+          <button className='checkbox' onClick={ () => collected(i) }>
+            {isCollected && <BsCheck/>}
+          </button>
+          <h1>( {amount === 0 ? '1' : amount} ) {title} - <span>{category}</span></h1>
+        </div>
 
-      <Options collected={isCollected} indexPage={index} indexEl={i} option={options}/>
-      
-    </Product>
+        <Options collected={isCollected} indexPage={index} indexEl={i} option={options} setOpenEdit={setOpenEdit}/>
+        
+      </Product>
+    </>
   );
 };
 
