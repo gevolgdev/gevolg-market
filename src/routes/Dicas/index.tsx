79483c2209c59c382c33a34ff775fe5dcddf85cf
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-03 20:51:24 -0300

    Routers: Contruindo paginas de novas rotas

---
+++ src/routes/Dicas/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageInBuilding from '../../components/PageInBuilding';
+import { Wrapper } from '../../style/GlobalStyle';
+
+const Dicas = () => {
+  return (
+    <Wrapper>
+      <PageInBuilding/>
+    </Wrapper>
+  );
+};
+
+export default Dicas;
---
+++ src/routes/FAQ/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageInBuilding from '../../components/PageInBuilding';
+
+const Faq = () => {
+  return (
+    <PageInBuilding/>
+  );
+};
+
+export default Faq;
