f96280d3a3805e0e1cc09a9baddceb5f0e335c0b
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-07 16:58:07 -0300

    DataInformations: Dados das instruções

---
+++ src/components/Initial/Instructions/dataInformations.ts
@@ -0,0 +1,28 @@
+import Image01 from '../../../assets/imgLeadMore_1.png';
+import Image02 from '../../../assets/imgLeadMore_2.png';
+import Image03 from '../../../assets/imgLeadMore_3.png';
+import Image04 from '../../../assets/imgLeadMore_4.png';
+import { InfosProps } from '../../../types/types';
+
+export const infos: InfosProps[] = [
+  {
+    title: 'Ir ao mercado nunca foi tão prático!',
+    image: Image01,
+    readmore: 'Gevolg Market, uma ideia que nasceu com o intuito de facilitar e ajudar as pessoas no momento em que elas estão fazendo compras no mercado. Sendo assim, ajudar na organização das listas de compras, fornecer dicas sobre como e o que comprar, e claro, deixando tudo isso salvo no app para sempre estar usando as suas listas criadas.',
+  },
+  {
+    title: 'Crie e configure sua lista.',
+    image: Image02,
+    readmore: 'Ao entrar no app, você verá um botão roxo de “Adicionar”, clicando nele você consegue estar configurar sua primeira lista. Nome da lista: Coloque aqui um nome que dará sentido à sua lista. Prioridade: Qual a importância das compras dessa lista? Cor: Adicione cores para categorizá-la ou por estética.',
+  },
+  {
+    title: 'Adicione um produto da sua compra.',
+    image: Image03,
+    readmore: 'Agora abra a lista e comece adicionar os produtos que você irá comprar no mercado. Nome: Qual o nome do produto? Quantidade e referência: Quantas unidades, quilos ou gramas você precisa levar? Categoria/Setor: Onde se encontra esse produto dentro do mercado?',
+  },
+  {
+    title: 'Interaja e organize os produtos.',
+    image: Image04,
+    readmore: 'Com o produto adicionado dentro da sua lista de compras você pode começar a administrar ele. Como gerenciar, Arquivar: Caso você precise deste produto na lista, porém em um dia específico você não irá precisar comprar ele, você pode arquivar o produto e deixar apenas os que você irá comprar naquele dia. Editar: Digitou errado ou mudou de ideia? Basta editar e pronto. Excluir: Esse é auto-explicativo, né? rs',
+  },
+]
