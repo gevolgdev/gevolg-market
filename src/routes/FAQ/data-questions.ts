7161d44841f1dff13acb77e9a177509e6c8dbe69
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-17 11:04:49 -0300

    data-questions: Perguntas e respostas

---
+++ src/routes/FAQ/data-questions.ts
@@ -0,0 +1,18 @@
+export const questions = [
+  {
+    title: 'O que é a Gevolg Market?',
+    answer: 'Gevolg Market, uma ideia que nasceu com o intuito de facilitar e ajudar as pessoas no momento em que elas estão fazendo compras no mercado. Sendo assim, ajudar na organização das listas de compras, fornecer dicas sobre como e o que comprar, e claro, deixando tudo isso salvo no app para sempre estar usando as suas listas criadas.',
+  },
+  {
+    title: 'Quando devo usar o app?',
+    answer: 'Resposta',
+  },
+  {
+    title: '?',
+    answer: 'Resposta',
+  },
+  {
+    title: '?',
+    answer: 'Resposta',
+  },
+];
