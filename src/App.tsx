5f5f7c6d9cfd7f3ba6d4cea42a695a508d08faa4
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-17 15:51:49 -0300

    App: correçao

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('../service-worker.js')
+      navigator.serviceWorker.register('/service-worker.js')
         .then(registration => {
           console.log('Service worker registrado com sucesso:', registration);
         })
