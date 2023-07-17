bccbab11de9e5430df785dc9bffb979d51dd4b0d
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-17 13:15:56 -0300

    App: Bloquear ao entrar pelo pc

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import { Outlet } from "react-router-dom";
 import { GlobalStyle, Wrapper } from './style/GlobalStyle';
 
 function App() {
+
+  const userScreen = window.innerWidth;
+
   return (
     <>
       <Wrapper>
-        <Outlet/>
+        {userScreen < 800  
+          ? <Outlet/> 
+          : <h1>Use seu celular</h1>
+        }
       </Wrapper>
+      
       <GlobalStyle/>
     </>
   )
