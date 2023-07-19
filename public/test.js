62b2b016b218f10bf862f1beff4a96408a3d081d
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-19 11:20:48 -0300

    VitePWA

---
+++ public/test.js
@@ -0,0 +1,44 @@
+// const CACHE_NAME = 'version-1';
+// const urlsToCache = [
+//   '/index.html',
+// ];
+
+// let deferredPrompt;
+
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(cache => {
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
+
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(response => {
+//         return response || fetch(event.request);
+//       })
+//       .catch(() => caches.match('offline.html'))
+//   );
+// });
+
+// self.addEventListener('activate', event => {
+//   const cacheWhiteList = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys()
+//       .then(cacheNames => Promise.all(
+//         cacheNames.map(item => {
+//           if (!cacheWhiteList.includes(item)) {
+//             return caches.delete(item);
+//           }
+//         })
+//       ))
+//   );
+// });
+
+// self.addEventListener('beforeinstallprompt', event => {
+//   event.preventDefault();
+//   deferredPrompt = event;
+// });
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,56 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'maskable.png', 'favicon-144.png', 'favicon-144.png', 'favicon-192.png', 'favicon-512.png'],
+      manifest: {
+        name: "Gevolg Market",
+        short_name: "Gevolg Market",
+        description: "Aplicativo para organizar e gerênciar listas de compras.",
+        display: "standalone",
+        background_color: "#9B7BFF",
+        theme_color: "#9B7BFF",
+        id: "/?source=pwa",
+        start_url: "https://gevolg-market.vercel.app/",
+        icons: [
+          {
+            src: "maskable.png",
+            type: "image/png",
+            sizes: "any",
+            purpose: "maskable"
+          },
+          {
+            src: "favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any"
+          },
+          {
+            src: "favicon-144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "favicon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "favicon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          }
+        ]
+      }
+    }),
+  ],
 })
